4dd8e6c8f5a9fb1e1b91f07edec47f0d3636b62b
harshpratap092507 <78509544+harshpratap092507@users.noreply.github.com>
2024-02-13 01:56:37 +0530

    initial commit

---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <Image
+                    src=""
+                    alt=""
+                    width={20}
+                    height={20}
+                />
+            </header>
+        </>
+    )
+}
+
+export default Header;
